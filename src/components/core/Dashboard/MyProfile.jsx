5efd56cc6dbdea7dd6555ad92873458f6a374e8c
Prathamesh0009M <161243490+Prathamesh0009M@users.noreply.github.com>
2024-11-04 14:48:04 +0530

    Update MyProfile.jsx explore

--- src/components/core/Dashboard/MyProfile.jsx
+++ src/components/core/Dashboard/MyProfile.jsx
@@ -146,7 +146,7 @@ const RecommendationCard = ({ recommendation }) => {
 
     return (
         <div className="bg-white rounded-lg p-4 shadow-md flex flex-col justify-between">
-            <div className="flex items-center mb-2">
+            <div className="flex flex-col md:flex items-center mb-2">
                 <img
                     src={recommendation.image}
                     alt={`${recommendation.firstName} ${recommendation.lastName}`}
