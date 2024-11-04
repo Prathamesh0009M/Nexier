2a46ed442426ccab33a18353392f28965da301ee
Prathamesh0009M <161243490+Prathamesh0009M@users.noreply.github.com>
2024-11-04 13:50:35 +0530

    Update Home.jsx mt

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     <div>
       {/* sction 1 */}
       <div className='relative mx-auto flex flex-col w-11/12 items-center max-w-maxContent text-white justify-between '>
-        <div className='w-full  h-auto mt-12  '>
+        <div className='w-full  h-auto mt-5  '>
 
           <UpdateSlider />
 
@@ -14,7 +14,7 @@ const Home = () => {
 
         
 
-        <p className='mt-12 text-3xl'>Fresh recommendations</p>
+        <p className='mt-8 text-3xl'>Fresh recommendations</p>
 
         <div className='mt-2'>
           <AllProducts />
