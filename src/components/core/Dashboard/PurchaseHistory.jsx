cc98693721bebcede844e45bfeed537fb124d313
Prathamesh0009M <161243490+Prathamesh0009M@users.noreply.github.com>
2024-12-27 23:41:20 +0530

    Create PurchaseHistory.jsx

---
+++ src/components/core/Dashboard/PurchaseHistory.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const PurchaseHistory = () => {
+    return (
+        <div className="flex items-center justify-center h-screen bg-gray-800">
+            <div className="bg-red-600 text-white text-center p-6 rounded-md shadow-md max-w-xs sm:max-w-md md:max-w-lg">
+                <h1 className="text-xl font-bold">Service Unavailable</h1>
+                <p className="mt-2">
+                    We apologize, but the service is currently unavailable due to some reason. Please try again later.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default PurchaseHistory
